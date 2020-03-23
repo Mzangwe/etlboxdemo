890326e017eb4fecdd5d11ba6c2e8b8e286400a2
anlennar <andreas.lennartz@akelius.de>
2020-03-23 11:21:25 +0100

    Renaming Basic example & improving code

--- ControlDataFlowBasics/Program.cs
+++ ControlDataFlowBasics/Program.cs
@@ -9,15 +9,18 @@ namespace ALE.BasicExample
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            //Set up the connection manager to master
             var masterConnection = new SqlConnectionManager("Data Source=.;Integrated Security=false;User=sa;password=reallyStrongPwd123");
+            //Recreate database 
             DropDatabaseTask.DropIfExists(masterConnection, "demo");
             CreateDatabaseTask.Create(masterConnection, "demo");
 
+            //Get connection manager to previously create database
             var dbConnection = new SqlConnectionManager("Data Source=.;Initial Catalog=demo;Integrated Security=false;User=sa;password=reallyStrongPwd123");
 
+            //Create destination table
             CreateTableTask.Create(dbConnection, "Table1", new List<TableColumn>()
             {
                 new TableColumn("ID","int",allowNulls:false, isPrimaryKey:true, isIdentity:true),
@@ -25,16 +28,21 @@ namespace ALE.BasicExample
                 new TableColumn("Col2","smallint",allowNulls:true)
             });
 
+            //Create dataflow for loading data from csv into table
             CsvSource<string[]> source = new CsvSource<string[]>("input.csv");
             RowTransformation<string[], MyData> row = new RowTransformation<string[], MyData>(
-            input => new MyData() { Col1 = input[0], Col2 = input[1] });
+                input => 
+                    new MyData() { Col1 = input[0], Col2 = input[1] }
+            );
             DbDestination<MyData> dest = new DbDestination<MyData>(dbConnection, "Table1");
 
+            //Link components & run data flow
             source.LinkTo(row);
             row.LinkTo(dest);
             source.Execute();
             dest.Wait();
 
+            //Check if data exists in destination
             SqlTask.ExecuteReader(dbConnection, "Read all data from table1",
             "select Col1, Col2 from Table1",
                 col1 => Console.WriteLine(col1.ToString() + ","),
@@ -43,6 +51,5 @@ namespace ALE.BasicExample
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
         }
-
     }
 }
